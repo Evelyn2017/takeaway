7e60aa300bfefafb7faa350a4e3ad72eb27ed157
Evelyn2017 <evelinx@foxmail.com>
2019-03-20 11:42:09 +0800

    use lombok to omit getter and setter

--- src/main/java/com/liu/dataobject/ProductCategory.java
+++ src/main/java/com/liu/dataobject/ProductCategory.java
@@ -1,55 +1,62 @@
 package com.liu.dataobject;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * 类目表
  */
 //@Table(name="product_category")
 @Entity
+@DynamicUpdate
+@Data
 public class ProductCategory {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer categoryId;
 
     private String categoryName;
 
     private Integer categoryType;
 
-    public Integer getCategoryId() {
-        return categoryId;
-    }
+    private Date createTime;
 
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
+    private Date updateTime;
 
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                '}';
-    }
+//    public Integer getCategoryId() {
+//        return categoryId;
+//    }
+//
+//    public void setCategoryId(Integer categoryId) {
+//        this.categoryId = categoryId;
+//    }
+//
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
+//
+//    public Integer getCategoryType() {
+//        return categoryType;
+//    }
+//
+//    public void setCategoryType(Integer categoryType) {
+//        this.categoryType = categoryType;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ProductCategory{" +
+//                "categoryId=" + categoryId +
+//                ", categoryName='" + categoryName + '\'' +
+//                ", categoryType=" + categoryType +
+//                '}';
+//    }
 }
--- src/test/java/com/liu/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/liu/repository/ProductCategoryRepositoryTest.java
@@ -7,18 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductCategoryRepositoryTest {
     @Autowired
     private ProductCategoryRepository repository;
 
-//    @Test
-//    public void findOneTest(){
-//        ProductCategory productCategory = repository.findOne()
-//        System.out.println(productCategory.toString());
-//    }
+    @Test
+    public void findOneTest(){
+        ProductCategory productCategory = repository.findById(1).get();
+        System.out.println(productCategory.toString());
+    }
+
+    @Test
+    public void saveTest() {
+        ProductCategory productCategory = repository.findById(2).get();
+        productCategory.setCategoryType(3);
+
+        repository.save(productCategory);
+    }
 
 }
