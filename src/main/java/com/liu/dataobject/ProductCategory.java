7c9fba8169497bac34bc8c436d2ff2faefbd3814
Evelyn2017 <evelinx@foxmail.com>
2019-03-20 14:04:51 +0800

    test select in table product_category.

--- src/main/java/com/liu/dataobject/ProductCategory.java
+++ src/main/java/com/liu/dataobject/ProductCategory.java
@@ -1,7 +1,12 @@
 package com.liu.dataobject;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -13,6 +18,7 @@ import java.util.Date;
 @Entity
 @DynamicUpdate
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class ProductCategory {
 
     @Id
@@ -23,11 +29,25 @@ public class ProductCategory {
 
     private Integer categoryType;
 
+    @Column(name="create_time")
+    @CreatedDate
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss")
     private Date createTime;
 
+    @Column(name="update_time")
+    @LastModifiedDate
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss")
     private Date updateTime;
 
-//    public Integer getCategoryId() {
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+
+    public ProductCategory() {
+    }
+
+    //    public Integer getCategoryId() {
 //        return categoryId;
 //    }
 //
--- src/test/java/com/liu/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/liu/repository/ProductCategoryRepositoryTest.java
@@ -1,30 +1,46 @@
 package com.liu.repository;
 
 import com.liu.dataobject.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableJpaAuditing
 public class ProductCategoryRepositoryTest {
     @Autowired
     private ProductCategoryRepository repository;
 
     @Test
-    public void findOneTest(){
+    public void findOneTest() {
         ProductCategory productCategory = repository.findById(1).get();
         System.out.println(productCategory.toString());
     }
 
     @Test
+    @Transactional
     public void saveTest() {
-        ProductCategory productCategory = repository.findById(2).get();
-        productCategory.setCategoryType(3);
+        ProductCategory productCategory = new ProductCategory("男最爱", 10);
+        ProductCategory result = repository.save(productCategory);
 
-        repository.save(productCategory);
+        Assert.assertNotNull(result);
+//        Assert.assertNotEquals(null, result);
+    }
+
+    @Test
+    public void findByCategoryInTest() {
+        List<Integer> list = Arrays.asList(2, 3, 4);
+        List<ProductCategory> result = repository.findByCategoryTypeIn(list);
+        Assert.assertNotEquals(0, result.size());
     }
 
 }
